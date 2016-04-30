c0d62adffe731f05f08d38dce7faa2277fefb2a4
Christian Höltje <docwhat@gerf.org>
2016-04-30 16:33:51 -0400

    More tests!

--- image/image.go
+++ image/image.go
@@ -17,46 +17,70 @@ type Interface interface {
 	ShortID() string
 	ShortParentID() string
 	IsOrphan() bool
+	HasTags() bool
+	HasDigests() bool
 }
 
 // Image provides methods for manipulating docker images.
 type Image struct {
-	image types.Image
+	types.Image
 }
 
 // New creates a new Image from docker images.
 func New(dockerImage types.Image) *Image {
-	return &Image{image: dockerImage}
+	return &Image{dockerImage}
 }
 
 // NewList creates a list of Images from docker images.
-func NewList(dockerImages []types.Image) []Interface {
-	images := make([]Interface, len(dockerImages))
+func NewList(dockerImages []types.Image) []Image {
+	images := make([]Image, len(dockerImages))
 
 	for i, dockerImage := range dockerImages {
-		images[i] = New(dockerImage)
+		images[i] = *New(dockerImage)
 	}
 	return images
 }
 
 // IsOrphan returns true if the image has no parents.
 func (i Image) IsOrphan() bool {
-	return "" == i.image.ParentID
+	return "" == i.ParentID
+}
+
+// HasTags returns true if any tags (other than `<none>:<none>`) are present.
+func (i Image) HasTags() bool {
+	switch len(i.RepoTags) {
+	case 0:
+		return false
+	case 1:
+		return i.RepoTags[0] != "<none>:<none>"
+	}
+	return true
+}
+
+// HasDigests returns true if any digests (other than `<none>@<none>`) are present.
+func (i Image) HasDigests() bool {
+	switch len(i.RepoDigests) {
+	case 0:
+		return false
+	case 1:
+		return i.RepoDigests[0] != "<none>@<none>"
+	}
+	return true
 }
 
 // AgeOf returns how long since the image was created.
 func (i Image) AgeOf() time.Duration {
-	return now.Sub(time.Unix(i.image.Created, 0))
+	return now.Sub(time.Unix(i.Created, 0))
 }
 
 // ShortID provides a shortened form of the ID digest.
 func (i Image) ShortID() string {
-	return shortenDigest(i.image.ID)
+	return shortenDigest(i.ID)
 }
 
 // ShortParentID provides a shortened form of the ParentID digest.
 func (i Image) ShortParentID() string {
-	return shortenDigest(i.image.ParentID)
+	return shortenDigest(i.ParentID)
 }
 
 func shortenDigest(digest string) string {
--- image/image_test.go
+++ image/image_test.go
@@ -12,9 +12,67 @@ func TestNew(t *testing.T) {
 	New(image)
 }
 
-func TestShortenDigest(t *testing.T) {
-	sha := "sha256:d8be53875f1e4d291c68a0b31a00a4eb2a171d37efdad3b551d1aa41da7841a6"
-	got := shortenDigest(sha)
-	want := "d8be538"
-	assert.Equal(t, want, got)
+// **********************************************
+// ShortID
+func TestNullShortID(t *testing.T) {
+	assert := assert.New(t)
+	i := Image{}
+
+	assert.Equal("", i.ShortID())
+
+	i.ID = "sha256:ffe2bc3a35a361004484b73c5797bd6254dc18cf93a5a48e5be7df784a71711e"
+	assert.Equal("ffe2bc3", i.ShortID())
+
+	i.ID = "fred"
+	assert.Equal("fred", i.ShortID())
+}
+
+func TestShortParentID(t *testing.T) {
+	assert := assert.New(t)
+	i := Image{}
+
+	assert.Equal("", i.ShortParentID())
+
+	i.ParentID = "sha256:d8be53875f1e4d291c68a0b31a00a4eb2a171d37efdad3b551d1aa41da7841a6"
+	assert.Equal("d8be538", i.ShortParentID())
+
+	i.ParentID = "fred"
+	assert.Equal("fred", i.ShortParentID())
+}
+
+func TestIsOrphan(t *testing.T) {
+	assert := assert.New(t)
+	i := Image{}
+
+	i.ParentID = ""
+	assert.True(i.IsOrphan())
+
+	i.ParentID = "sha256:5b4e987b9b0945abcef0ddb745fd70f8f3110a34a3424f215c62c134deecb80f"
+	assert.False(i.IsOrphan())
+}
+
+func TestHasTags(t *testing.T) {
+	assert := assert.New(t)
+	i := Image{}
+
+	assert.False(i.HasTags())
+
+	i.RepoTags = append(i.RepoTags, "fred:1.2.3")
+	assert.True(i.HasTags())
+
+	i.RepoTags[0] = "<none>:<none>"
+	assert.False(i.HasTags(), "<none>:<none> should not be considered a tag")
+}
+
+func TestHasDigests(t *testing.T) {
+	assert := assert.New(t)
+	i := Image{}
+
+	assert.False(i.HasDigests())
+
+	i.RepoDigests = append(i.RepoDigests, "fred@foobar")
+	assert.True(i.HasDigests(), "There is a digest so it should be true")
+
+	i.RepoDigests[0] = "<none>@<none>"
+	assert.False(i.HasDigests(), "<none>@<none> should not be considered a digest")
 }
